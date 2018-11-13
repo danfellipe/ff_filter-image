358d35443ac9e0c4f61ede0be34b71d15ffc0a1f
D. Fellipe de Palma <dfellipe7@gmail.com>
2018-11-13 20:42:07 -0200

    Upload arquivo inicial

---
+++ ff_filter-image.php
@@ -0,0 +1,14 @@
+<?php
+defined ('ABSPATH') or die ('No script kiddies please!');
+/*
+  Plugin Name: Filtro Para Imagens
+  Plugin URI: https://github.com/danfellipe/ff_filter-image/
+  Description: Filtro Para Imagens
+  Version: 1.0
+  Author: Fellipe de Palma & Felipe Soares
+  Author URI: https://github.com/danfellipe/
+  License: MIT
+*/
+
+
+?>
