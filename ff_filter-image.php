75d3ad3b95b7f97a1d7e94fd62335e6ea2ba8e6b
Fellipe de Palma <dfellipe7@gmail.com>
2018-11-13 21:37:30 -0200

    Update ff_filter-image.php
    add function ff_filter_image

--- ff_filter-image.php
+++ ff_filter-image.php
@@ -10,5 +10,19 @@ defined ('ABSPATH') or die ('No script kiddies please!');
   License: MIT
 */
 
+function ff_filter_image($atts){
+
+  $html  = '<form class="" action="" method="post">';
+  $html .= '<input type="file" name="upload-img" accept="image/png, image/jpeg"/>';
+  $html .= '</form>';
+  return $html;
+}
+
+add_shortcode( 'filter-image', 'ff_filter_image' );
 
 ?>
+
+<script>
+
+
+</script>
