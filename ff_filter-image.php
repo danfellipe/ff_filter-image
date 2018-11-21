7af02b1bc37545cb5f549e96ab628aa74b18030a
Fellipe de Palma <dfellipe7@gmail.com>
2018-11-21 18:31:41 -0200

    Apresentação Professor

--- ff_filter-image.php
+++ ff_filter-image.php
@@ -2,32 +2,28 @@
 defined ('ABSPATH') or die ('No script kiddies please!');
 
 /*
-  Plugin Name: Filtro Para Imagens
+  Plugin Name: Filtro para Imagens
   Plugin URI: https://github.com/danfellipe/ff_filter-image/
-  Description: Filtro Para Imagens
+  Description: Filtro para Imagens
   Version: 1.0
   Author: Fellipe de Palma & Felipe Soares
   Author URI: https://github.com/danfellipe/
   License: MIT
 */
 
-
 function ff_filter_image($atts){
-
-  $html  = '<form class="" action="" method="post" id="fileSelect">';
-  $html .= '<label for="fileElem" class="control-label label-bordered">CLIQUE AQUI PARA ESCOLHER UM ARQUIVO</label>';
-  $html .= '<input type="file" id="fileElem" name="upload-img" accept="image/png, image/jpeg"/>';
-  $html .= '<div id="fileList">';
-  //$html .= '<div id="fileList">Escolha uma imagem!</p>';
-  $html .= '</div>';
-  $html .= '<script src="' .  plugins_url('ff_filter-image/main.js', dirname(__FILE__)) .'">';
-  $html .= '</form>';
-  return $html;
+    include_once( 'controles.html' );
+    $js = '<script src="' .  plugins_url('ff_filter-image/main.js', dirname(__FILE__)) .'">';
+    return $js;
 }
-
 add_shortcode( 'filter-image', 'ff_filter_image' );
+
+// Inclui estilos necessários ao plugin
+function add_css() {
+    $arquivo_css = plugins_url( 'ff_filter_image.css', __FILE__ );
+    if ( is_front_page() ){
+        wp_enqueue_style ( 'style', $arquivo_css );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'add_css' );
 ?>
-
-<script>
-
-</script>
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// UPLOAD COM PREVIEW
 window.URL = window.URL || window.webkitURL;
 
 var fileSelect = document.getElementById("fileSelect"),
@@ -7,21 +8,74 @@ var fileSelect = document.getElementById("fileSelect"),
 fileElem.addEventListener('change', handleFiles)
 
 function handleFiles(event) {
-  var files = this.files
+    var files = this.files
 
-  if (!files.length) {
-    fileList.innerHTML = "<p>Nenhuma Imagem Selecionada!</p>";
-  } else {
-    fileList.innerHTML = "";
-    var div = document.createElement("div");
-    fileList.appendChild(div);
+    if (!files.length) {
+        fileList.innerHTML = "<p>Nenhuma Imagem Selecionada!</p>";
+    } else {
+        fileList.innerHTML = "";
+        var div = document.createElement("div");
+        fileList.appendChild(div);
 
-    var img = new Image();
-    img.src = window.URL.createObjectURL(files[0]);
-    img.height = 60;
-    img.onload = function() {
-      window.URL.revokeObjectURL(this.src);
+        var img = new Image();
+        img.src = window.URL.createObjectURL(files[0]);
+        img.height = 60;
+        img.onload = function() {
+            window.URL.revokeObjectURL(this.src);
+        }
+        div.appendChild(img);
     }
-    div.appendChild(img);
-  }
+}
+
+// FILTROS
+//Filtro Sepia
+function setSepia(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "sepia(" + e.value + ")";
+}
+
+//Filtro brightness
+function setBrightness(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "brightness(" + e.value + ")";
+}
+
+//Filtro brightness
+function setContrast(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "contrast(" + e.value + ")";
+}
+
+//Filtro Grayscale
+function setGrayscale(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "grayscale(" + e.value + ")";
+}
+
+//Filtro Invert
+function setInvert(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "invert(" + e.value + ")";
+}
+
+//Filtro Opacity
+function setOpacity(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "opacity(" + e.value + ")";
+}
+
+//Filtro Saturate
+function setSaturate(e) {
+    document.getElementById('fileList').style["webkitFilter"] = "saturate(" + e.value + ")";
+}
+
+//MOSTRAR FILTROS / OCULTAR
+function mudarEstado(el){
+    var b1 = document.getElementById('bt1').style.display;
+    var b2 = document.getElementById('bt2').style.display;
+    var b3 = document.getElementById('bt3').style.display;
+    var b4 = document.getElementById('bt4').style.display;
+    var b5 = document.getElementById('bt5').style.display;
+    var b6 = document.getElementById('bt6').style.display;
+    var b7 = document.getElementById('bt7').style.display;
+
+    if (b1 == "none"){
+
+    }
+
+
 }
